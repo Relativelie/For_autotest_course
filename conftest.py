caf8027c11f460acebca8f72842cac76d7d0da77
Relativelie <sopio1992@gmail.com>
2020-01-30 10:37:11 +0300

    Исправила ошибку с языком

---
+++ conftest.py
@@ -0,0 +1,14 @@
+import pytest
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+
+@pytest.fixture(scope="function")
+def browser():
+    print("\nstart browser for test..")
+    # browser = webdriver.Chrome()
+    options = Options()
+    options.add_experimental_option('prefs', {'intl.accept_languages': 'en'})
+    browser = webdriver.Chrome(options=options)
+    yield browser
+    print("\nquit browser..")
+    browser.quit()
